3d820fb7a8e3ac2a01003a8bca2033c1f7a3801d
Pratik Sarvaiya <sarvaiyapratik09@gmail.com>
2024-12-17 16:26:24 +0530

    mongodb connection

---
+++ src/db/db.connection.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+import {DB_NAME} from "../constants.js"
+
+const connectDB = async () => {
+    try {
+
+        // console.log(process.env.MONGODB_URI)
+        
+        const response = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`MongoDB Connected !! \nRunning on port ${process.env.PORT}`)
+
+    } catch (error) {
+        console.error("My Error connecting MONGODB : ", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
